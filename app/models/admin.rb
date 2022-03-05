7dbef59ce847d1c169e52f2a4261725aea659c83
monicaarrambide <55818711+monicaarrambide@users.noreply.github.com>
2022-03-05 15:21:27 -0600

    Testing with admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,4 +5,24 @@ class Admin < ApplicationRecord
     return nil unless /@gmail.com || @tamu.edu\z/.match?(email)
     create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
+
+  def current_user
+    @current_user ||= User.find(session[:email]) if session[:email]
+  end
+
+  def signed_in?
+      !!current_user
+  end
+
+  def is_admin?
+    signed_in? ? current_user.admin : false
+  end
+
+  def self.current_user_admin?(current_admin)
+    user = Admin.where(email: current_admin.email).first
+
+    permission = User.where(email: current_admin.email, isAdmin: true, isOfficer: true)
+
+    !permission.nil?
+  end
 end
